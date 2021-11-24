f8aeaf8b8b9478368ed1fb88ff5f7a63f20dc752
joy.chen <joy.chen@justalk.com>
2021-11-24 11:38:41 +0800

    添加注解及标准的作用域

--- library/src/main/java/org/justalk/kotlin/stdlib/app/Fragment.kt
+++ library/src/main/java/org/justalk/kotlin/stdlib/app/Fragment.kt
@@ -7,6 +7,7 @@ import kotlin.properties.ReadOnlyProperty
 /**
  * ViewDataBinding 代理
  */
+@Suppress("unused")
 inline fun <reified T : ViewDataBinding> Fragment.viewDataBindingDelegate(): ReadOnlyProperty<Fragment, T> {
     return FragmentViewDataBindingProperty()
 }
--- library/src/main/java/org/justalk/kotlin/stdlib/app/FragmentViewDataBindingProperty.kt
+++ library/src/main/java/org/justalk/kotlin/stdlib/app/FragmentViewDataBindingProperty.kt
@@ -3,6 +3,7 @@ package org.justalk.kotlin.stdlib.app
 import android.os.Handler
 import android.os.Looper
 import androidx.annotation.MainThread
+import androidx.annotation.RestrictTo
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
@@ -11,6 +12,7 @@ import androidx.lifecycle.LifecycleOwner
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
+@RestrictTo(RestrictTo.Scope.LIBRARY)
 class FragmentViewDataBindingProperty<T : ViewDataBinding> : ReadOnlyProperty<Fragment, T> {
 
     private var viewBinding: T? = null
