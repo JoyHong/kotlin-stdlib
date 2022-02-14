bb9eb011bb2e8abcc7261c1a27b0fca4fda52430
joy.chen <joy.chen@justalk.com>
2022-02-14 12:33:40 +0800

    Revert "Fragment getArgument 封装代理" (不够灵活)
    This reverts commit 1f0180bab0ce15b897c6ccc08cc0115e8b2191d1.

--- app/src/main/java/org/justalk/kotlin/demo/MainActivity.kt
+++ app/src/main/java/org/justalk/kotlin/demo/MainActivity.kt
@@ -3,33 +3,24 @@ package org.justalk.kotlin.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import org.justalk.kotlin.demo.databinding.FragmentMainBinding
-import org.justalk.kotlin.stdlib.app.argumentStringDelegate
 import org.justalk.kotlin.stdlib.app.viewDataBindingDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, MainFragment()
-                .apply {
-                    arguments = bundleOf("key_string" to "test")
-                })
-            .commitAllowingStateLoss()
     }
 }
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val binding: FragmentMainBinding by viewDataBindingDelegate()
-    private val keyString: String by argumentStringDelegate("key_string")
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.text1.text = "Hello viewDataBindingDelete:$keyString"
+        binding.text1.text = "Hello viewDataBindingDelete"
     }
 
 }
