8f7de41c2829eae9fa0ded5c4f7a5be586c6b457
joy.chen <joy.chen@justalk.com>
2024-05-24 17:36:22 +0800

    refactor: 完善 sample 代码

--- app/src/main/java/org/justalk/kotlin/demo/MainActivity.kt
+++ app/src/main/java/org/justalk/kotlin/demo/MainActivity.kt
@@ -1,16 +1,20 @@
 package org.justalk.kotlin.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.justalk.kotlin.demo.databinding.FragmentMainBinding
+import org.justalk.kotlin.stdlib.activity.hideIme
+import org.justalk.kotlin.stdlib.activity.setImmersiveMode
+import org.justalk.kotlin.stdlib.activity.showIme
 import org.justalk.kotlin.stdlib.app.viewDataBindingDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setImmersiveMode()
     }
 }
 
@@ -18,9 +22,18 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val binding: FragmentMainBinding by viewDataBindingDelegate()
 
+    private var isSystemBarShowing = true
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.text1.text = "Hello viewDataBindingDelete"
+        binding.text1.setOnClickListener {
+            if (isSystemBarShowing) {
+                requireActivity().showIme()
+            } else {
+                requireActivity().hideIme()
+            }
+            isSystemBarShowing = !isSystemBarShowing
+        }
     }
 
 }
