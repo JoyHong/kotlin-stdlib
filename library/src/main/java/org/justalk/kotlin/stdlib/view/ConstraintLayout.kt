93dd9fdc24c2754b78b47f63508d3c27870150bc
Joy.Chen <joy.chen@justalk.com>
2025-01-10 10:04:56 +0800

    refactor: 支持约束布局 expandChildView

--- library/src/main/java/org/justalk/kotlin/stdlib/view/ConstraintLayout.kt
+++ library/src/main/java/org/justalk/kotlin/stdlib/view/ConstraintLayout.kt
@@ -1,5 +1,6 @@
 package org.justalk.kotlin.stdlib.view
 
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 
@@ -8,5 +9,14 @@ inline fun ConstraintLayout.updateConstraintSet(block: ConstraintSet.() -> Unit)
         constraintSet.clone(this)
         block(constraintSet)
         constraintSet.applyTo(this)
+    }
+}
+
+fun ConstraintLayout.expandChildView(childView: View) {
+    updateConstraintSet {
+        connect(childView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
+        connect(childView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
+        connect(childView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
+        connect(childView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
     }
 }
