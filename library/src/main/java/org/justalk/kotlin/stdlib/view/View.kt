c1792819316d22a15d7e1c648ff56e8387f9acd9
Joy.Chen <joy.chen@justalk.com>
2025-05-16 11:53:12 +0800

    refactor: deprecated doOnGlobalLayout

--- library/src/main/java/org/justalk/kotlin/stdlib/view/View.kt
+++ library/src/main/java/org/justalk/kotlin/stdlib/view/View.kt
@@ -84,9 +84,7 @@ fun View.isLayoutRtl(): Boolean {
     return ViewCompat.getLayoutDirection(this) == ViewCompat.LAYOUT_DIRECTION_RTL
 }
 
-/**
- * 区别于 doOnLayout, doOnLayout 回调并不保证视图已经完全测量和布局好, 所以此时获取的 View 的宽高可能还是为 0
- */
+@Deprecated("建议都使用 doOnLayout, 在回调里判断 view 的宽高是否有效, 无效则丢弃回调", ReplaceWith("doOnLayout"))
 inline fun View.doOnGlobalLayout(crossinline action: (view: View) -> Unit) {
     viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
         override fun onGlobalLayout() {
