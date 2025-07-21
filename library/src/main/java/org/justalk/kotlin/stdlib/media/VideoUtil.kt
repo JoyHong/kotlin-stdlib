d7ef28f08f9102b6ca097ea4e2cf770aee0a57c3
luoyi <>
2025-07-21 09:47:57 +0800

    refactor:视频faststart转换优化异常处理

--- library/src/main/java/org/justalk/kotlin/stdlib/media/VideoUtil.kt
+++ library/src/main/java/org/justalk/kotlin/stdlib/media/VideoUtil.kt
@@ -6,6 +6,7 @@ import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator
 import org.justalk.kotlin.stdlib.Utils.inMainThread
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import java.nio.channels.WritableByteChannel
 
 object VideoUtil {
@@ -16,6 +17,8 @@ object VideoUtil {
      * @param inputFile 输入视频文件
      * @param outputFile 输出视频文件
      * @return 如果操作成功返回 true，否则返回 false
+     * @throws IOException 如果处理过程中发生I/O错误
+     * @throws RuntimeException 如果文件重命名失败
      */
     fun fastStart(inputFile: File, outputFile: File) {
         assert(!inMainThread) {
