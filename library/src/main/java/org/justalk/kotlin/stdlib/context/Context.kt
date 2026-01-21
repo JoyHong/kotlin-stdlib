e83f4e0d9ad78fcc65d7fe8c08593fd9cfc5c1cd
Joy.Chen <joy.chen@justalk.com>
2026-01-21 14:59:34 +0800

    refactor: 支持打开系统的下载管理文件夹

--- library/src/main/java/org/justalk/kotlin/stdlib/context/Context.kt
+++ library/src/main/java/org/justalk/kotlin/stdlib/context/Context.kt
@@ -1,11 +1,12 @@
 package org.justalk.kotlin.stdlib.context
 
 import android.app.Activity
+import android.app.DownloadManager
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.Intent
 import android.content.res.Configuration
-import android.util.TypedValue
-import androidx.annotation.Dimension
 
 /**
  * 返回上下文对应的 Activity, 如果有的话
@@ -27,4 +28,16 @@ fun Context.getActivity(): Activity? {
  */
 fun Context.hasHardwareKeyboard(): Boolean {
     return resources.configuration.keyboard == Configuration.KEYBOARD_QWERTY
+}
+
+/**
+ * 打开系统的 "下载管理" 文件夹下
+ */
+@Throws(ActivityNotFoundException::class)
+fun Context.openSystemDownloads() {
+    val intent = Intent(DownloadManager.ACTION_VIEW_DOWNLOADS)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    // 某些 ROM (如小米/华为旧版) 需要清除任务栈顶才能正确显示，否则可能白屏或无反应
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+    startActivity(intent)
 }
