942c3e005ee1eb4c2e7ec591baad61088e97e3e2
Joy.Chen <joy.chen@justalk.com>
2026-01-27 18:00:59 +0800

    refactor: 扩展函数 formatSize

--- library/src/main/java/org/justalk/kotlin/stdlib/io/FileSize.kt
+++ library/src/main/java/org/justalk/kotlin/stdlib/io/FileSize.kt
@@ -1,5 +1,7 @@
 package org.justalk.kotlin.stdlib.io
 
+import java.util.Locale
+
 // 1 KB (Kilobyte)
 inline val Int.kb: Long
     get() = this * 1024L
@@ -11,3 +13,25 @@ inline val Int.mb: Long
 // 1 GB (Gigabyte)
 inline val Int.gb: Long
     get() = this * 1024 * 1024 * 1024L
+
+// 1.0 KB (Kilobyte)
+inline val Double.kb: Long
+    get() = (this * 1024L).toLong()
+
+// 1.0 MB (Megabyte)
+inline val Double.mb: Long
+    get() = (this * 1024 * 1024L).toLong()
+
+// 1.0 GB (Gigabyte)
+inline val Double.gb: Long
+    get() = (this * 1024 * 1024 * 1024L).toLong()
+
+fun Long.formatSize(): String {
+    val bytes = this.toDouble()
+    return when {
+        bytes >= 1.gb -> String.format(Locale.US, "%.1f GB", bytes / 1.gb)
+        bytes >= 1.mb -> String.format(Locale.US, "%.1f MB", bytes / 1.mb)
+        bytes >= 1.kb -> String.format(Locale.US, "%.1f KB", bytes / 1.kb)
+        else -> String.format(Locale.US, "%.0f B", bytes)
+    }
+}
